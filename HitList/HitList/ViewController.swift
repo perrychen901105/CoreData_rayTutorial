54a978b2f84ba8c31f0ab33f44dc9d82cc9af299
Perry <chenzhipeng901105@gmail.com>
2016-03-03 23:04:23 +0800

    finish chapter one

---
+++ HitList/HitList/ViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by PerryChen on 3/3/16.
+//  Copyright © 2016 PerryChen. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+//    var names = [String]()
+    var people = [NSManagedObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "\"The List\""
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 1
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        // 2
+        let fetchRequest = NSFetchRequest(entityName: "Person")
+        
+        // 3
+        do {
+            let results =
+                try managedContext.executeFetchRequest(fetchRequest)
+            people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
+    
+    @IBAction func addName(sender: AnyObject) {
+        
+        
+        let alert = UIAlertController(title: "New Name",
+            message: "Add a new name",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default,
+            handler: { (action:UIAlertAction) -> Void in
+                
+                let textField = alert.textFields!.first
+//                self.names.append(textField!.text!)
+                self.saveName(textField!.text!)
+                self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
+        
+    }
+    
+    func saveName(name: String) {
+        // 1
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        // 2
+        /// An entity description is the piece that links the entity definition from your data model with an instance of NSManagedObject at runtime.
+        let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext)
+        
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        // 3
+        person.setValue(name, forKey: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+            // 5
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        
+        let person = people[indexPath.row]
+        
+        cell!.textLabel!.text = person.valueForKey("name") as? String
+        
+        return cell!
+    }
+    
+}
+
