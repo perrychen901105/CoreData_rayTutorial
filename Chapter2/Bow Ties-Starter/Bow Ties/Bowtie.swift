759dc44322b4f6a742737d60ef379ecc1630a693
Perry <chenzhipeng901105@gmail.com>
2016-03-04 03:48:58 +0800

    add

---
+++ Chapter2/Bow Ties-Starter/Bow Ties/Bowtie.swift
@@ -0,0 +1,17 @@
+//
+//  Bowtie.swift
+//  Bow Ties
+//
+//  Created by PerryChen on 3/3/16.
+//  Copyright © 2016 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Bowtie: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
